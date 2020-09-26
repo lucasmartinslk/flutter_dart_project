873d518ee17fcd93ce2415475a6657c36f5af7df
Lucas Martins <lucas1993x@hotmail.com>
2020-09-26 16:36:38 -0300

    Final-start_project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'dart:ui';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-
+import 'package:flutter_app/utils/componentes.dart';
 
 
 void main() => runApp(MyApp());
@@ -10,11 +9,17 @@ void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
-
 }
 
 class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   AnimationController _controller;
+
+  TextEditingController controllerPeso = TextEditingController();
+  TextEditingController controllerAltura = TextEditingController();
+  GlobalKey<FormState> cForm = GlobalKey<FormState>();
+  String info = "Informe os seus Dados";
+  String colorsx = 'green';
+
   @override
   void initState() {
     _controller = AnimationController(vsync: this);
@@ -26,48 +31,99 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
     _controller.dispose();
     super.dispose();
   }
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: paginaPrincipal(),
+      home: criaPagina(),
+
     );
   }
 
-  paginaPrincipal() {
+  criaPagina() {
     return Scaffold(
-      appBar: barraPrincipal(),
-      body: Container(
-        color: Colors.blueGrey,
-        child: textoPrincipal(),
-
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,),
-    );
-  }
-
-  barraPrincipal() {
-      return AppBar(
-        title: Text("Ola Flutter"),
+      appBar: AppBar(
+        title: Text("Calcula IMC"),
         centerTitle: true,
-        backgroundColor: Colors.pinkAccent,
-      );
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: resetaCampos,
+          ),
+        ],
+      ),
+      body: criaFormulario(),
+    );
   }
 
-  textoPrincipal() {
-    return Text(
-        "Texto Principal",
-        style: TextStyle(
-          color: Colors.blue,
-            fontSize: 50,
-            fontWeight: FontWeight.bold,
+  criaFormulario() {
+    return Form(
+      key: cForm,
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Componentes.caixa("Peso", "Digite o Peso", controllerPeso, validaPeso, numero: true),
+            Componentes.caixa("Altura", "Digite a altura", controllerAltura, validaAltura, numero: true),
+            Componentes.botao("Calcula", calcularImc),
+            Text(
+              info,
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+          ],
+        )
       ),
     );
+  }
+  Function validaPeso = ((value){
+    if(value.isEmpty){
+      return "Informe o Peso";
+    }
+    if(int.parse(value) <=0){
+      return "O Peso não pode ser negativo";
+    }
+    return null;
+  });
+
+  Function validaAltura = ((value){
+    if(value.isEmpty){
+      return "Informe a Altura";
+    }
+    if(double.parse(value) <=0){
+      return "A altura não pode ser negativa.";
+    }
+    return null;
+  });
+
+  resetaCampos(){
+    controllerPeso.text ="";
+    controllerAltura.text ="";
+    setState(() {
+      info = "Informe os seus dados";
+      cForm = GlobalKey<FormState>();
+    });
+  }
+
+  calcularImc(){
+    setState(() {
+    if(!cForm.currentState.validate()){
+      return null;
+    }
+    double p = double.parse(controllerPeso.text);
+    double a = double.parse(controllerAltura.text);
+    double imc = p/(a*a);
+    if (imc < 18.5)
+      info = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
+      colorsx = 'blue';
+    if (imc >= 18.5 && imc < 30)
+      info = "Peso Ideal (${imc.toStringAsPrecision(4)})";
+    else
+      info = "Acima do Peso (${imc.toStringAsPrecision(4)})";
+      colorsx = 'red';
+
+    print(info);
+    });
 
   }
 }
-
-
-
